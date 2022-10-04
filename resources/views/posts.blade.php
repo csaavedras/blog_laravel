b64bd01af099a3394f1e0a97a89f206c56d6f42b
cisaa <ci.saavedras@gmail.com>
2022-10-03 21:39:23 -0300

    Setup project

---
+++ resources/views/posts.blade.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+
+<title>My Blog </title>
+<link rel="stylesheet" href="/app.css">
+
+<body>
+    <article>
+        <h1><a href="/posts/my-first-post">My First Post</a></h1>
+
+        <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore deserunt doloribus dignissimos architecto, deleniti esse! Distinctio numquam provident accusantium culpa amet vel aliquid, iste voluptatum iusto eum officia vero nostrum.
+            Numquam vel inventore modi impedit dolorum laboriosam ea assumenda earum ipsam aut consectetur nesciunt ducimus harum corrupti amet nam, excepturi maxime. Accusantium architecto exercitationem, sit animi quos inventore repudiandae aliquid!
+            Ipsum libero repellat ea eum esse repellendus nam, nesciunt nihil id, accusantium officiis incidunt velit et animi aperiam enim earum quod, consectetur laborum exercitationem! Dignissimos illo ipsam fuga quasi laborum?
+            Officia nesciunt, laboriosam rem eaque exercitationem reprehenderit consequuntur provident ducimus veniam quasi, molestias illum id possimus voluptatibus sapiente. Explicabo maxime aspernatur rerum ipsum ullam corporis iure quidem doloremque odit quibusdam?
+            Laboriosam harum non iusto sit tempora atque odio perferendis fugiat hic ut magni nobis eos, vitae dolorum at necessitatibus ullam aut porro ratione. Accusantium sequi officiis numquam placeat minus! Magni.
+            Molestiae, tenetur minima, reiciendis ipsam quae, illum deserunt qui non provident beatae iusto voluptatibus nulla distinctio ea vitae cupiditate. Nemo quasi architecto perspiciatis maiores beatae dolore minus! Provident, quo nisi.
+        </p>
+    </article>
+
+    <article>
+        <h1><a href="/posts/my-second-post">My Seconds Post</a></h1>
+
+        <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore deserunt doloribus dignissimos architecto, deleniti esse! Distinctio numquam provident accusantium culpa amet vel aliquid, iste voluptatum iusto eum officia vero nostrum.
+            Numquam vel inventore modi impedit dolorum laboriosam ea assumenda earum ipsam aut consectetur nesciunt ducimus harum corrupti amet nam, excepturi maxime. Accusantium architecto exercitationem, sit animi quos inventore repudiandae aliquid!
+            Ipsum libero repellat ea eum esse repellendus nam, nesciunt nihil id, accusantium officiis incidunt velit et animi aperiam enim earum quod, consectetur laborum exercitationem! Dignissimos illo ipsam fuga quasi laborum?
+            Officia nesciunt, laboriosam rem eaque exercitationem reprehenderit consequuntur provident ducimus veniam quasi, molestias illum id possimus voluptatibus sapiente. Explicabo maxime aspernatur rerum ipsum ullam corporis iure quidem doloremque odit quibusdam?
+            Laboriosam harum non iusto sit tempora atque odio perferendis fugiat hic ut magni nobis eos, vitae dolorum at necessitatibus ullam aut porro ratione. Accusantium sequi officiis numquam placeat minus! Magni.
+            Molestiae, tenetur minima, reiciendis ipsam quae, illum deserunt qui non provident beatae iusto voluptatibus nulla distinctio ea vitae cupiditate. Nemo quasi architecto perspiciatis maiores beatae dolore minus! Provident, quo nisi.
+        </p>
+    </article>
+    <article>
+        <h1><a href="/posts/my-third-post">My Third Post</a></h1>
+
+        <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore deserunt doloribus dignissimos architecto, deleniti esse! Distinctio numquam provident accusantium culpa amet vel aliquid, iste voluptatum iusto eum officia vero nostrum.
+            Numquam vel inventore modi impedit dolorum laboriosam ea assumenda earum ipsam aut consectetur nesciunt ducimus harum corrupti amet nam, excepturi maxime. Accusantium architecto exercitationem, sit animi quos inventore repudiandae aliquid!
+            Ipsum libero repellat ea eum esse repellendus nam, nesciunt nihil id, accusantium officiis incidunt velit et animi aperiam enim earum quod, consectetur laborum exercitationem! Dignissimos illo ipsam fuga quasi laborum?
+            Officia nesciunt, laboriosam rem eaque exercitationem reprehenderit consequuntur provident ducimus veniam quasi, molestias illum id possimus voluptatibus sapiente. Explicabo maxime aspernatur rerum ipsum ullam corporis iure quidem doloremque odit quibusdam?
+            Laboriosam harum non iusto sit tempora atque odio perferendis fugiat hic ut magni nobis eos, vitae dolorum at necessitatibus ullam aut porro ratione. Accusantium sequi officiis numquam placeat minus! Magni.
+            Molestiae, tenetur minima, reiciendis ipsam quae, illum deserunt qui non provident beatae iusto voluptatibus nulla distinctio ea vitae cupiditate. Nemo quasi architecto perspiciatis maiores beatae dolore minus! Provident, quo nisi.
+        </p>
+    </article>
+
+</body>
